84520cc6e9571b0083dcaa9072c6fc2cf97c835c
DJ Coleman <DJ@DJs-MacBook-Pro.local>
2019-11-08 19:31:20 -0800

    Added react for stretch

---
+++ client/src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Checkout posts and actions</h1>
+    </div>
+  );
+}
+
+export default App;
